6192bb9da4978c1c54f852fab62cf412ca997032
Sachidanand Shah <s.shahmlg@gmail.com>
2022-09-01 11:56:57 +0530

    Config added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const PORT = 3000;
+
+
+const app = express();
+
+//------------ EJS Configuration ------------//
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+app.set('view engine', 'ejs');
+
+
+app.use('/', require('./routes/index'));
+
+app.listen(PORT, console.log(`Server Running On PORT ${PORT}`));
+
+
+
+
+
